8343539785de1b31190ada4d18a40ad9fa13b8b6
Frederik91 <frederik.t91@gmail.com>
2016-11-27 22:27:31 +0100

    Add files via upload

---
+++ Revisions/AddPanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace Revisions
+{
+    public class AddPanel : Autodesk.Revit.UI.IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // add new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("Revisions");
+
+            //Create a push button in the ribbon panel "NewRibbonPanel"
+            //the add-in application "HelloWorld" will be triggered when button is pushed
+
+            PushButton pushButton = ribbonPanel.AddItem(new PushButtonData("Create snapshot",
+                "Create snapshot", @"D:\HelloWorld.dll", "HelloWorld.CsHelloWorld")) as PushButton;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
